6ebeb633f7b0d57312742fa4906fe3f60f1224c6
Antonio <102518682+AntonioVispi@users.noreply.github.com>
2022-11-15 11:54:47 +0100

    Add files via upload

---
+++ Clean_Balancing.py
@@ -0,0 +1,452 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 10:55:10 2022
+
+@author: Antonio Vispi
+"""
+
+import os
+import argparse
+import albumentations as A
+import skimage.io as img
+import cv2
+import random
+from skimage.color import rgb2gray
+from skimage.transform import resize
+import numpy as np
+
+
+
+def counter(directory):
+  k=0
+  for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    k=k+1
+  return k
+
+def Sharpness (path_in,path_out):
+  
+  os.makedirs(path_out, exist_ok = True)
+
+  # Augmentation pipeline statement: Soft Sharpen
+
+  transform = A.Compose([
+          A.Sharpen (alpha=(0.2, 0.2), lightness=(1, 1), always_apply=True, p=1)])
+    
+  directory = os.fsencode(path_in)
+  k=0
+  for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    image = img.imread(path_in+'/'+filename)
+
+    # ----- Transformation ------
+ 
+    transformed = transform(image=image)
+    transformed_image = transformed["image"]
+
+    # ---------------------------
+
+    img.imsave(path_out+'/'+filename,transformed_image)
+    k = k+1  
+    
+  print(str(k)+' images of the current class have been saved.')
+
+def convert(img, target_type_min, target_type_max, target_type):
+    imin = img.min()
+    imax = img.max()
+
+    a = (target_type_max - target_type_min) / (imax - imin)
+    b = target_type_max - a * imax
+    new_img = (a * img + b).astype(target_type)
+    return new_img
+
+def telescope_test(image):
+
+  img = rgb2gray(image)
+  img = resize(img, (512, 512))  # make all the images square
+  img = convert(img, 0, 255, np.uint8)
+
+  mask_1 = np.ones([512,512], dtype=np.uint8)
+  mask_1[15:len(mask_1)-15,15:len(mask_1)-15] = 0  #mask consisting of the outer frame 15 pixels wide
+  norm_1 = np.sum(mask_1) #number of non-zero pixels of the thin mask
+  immagine_1 = img * mask_1;   # I just look at the picture frame, the rest is 0
+
+  mask_2 = np.ones([512,512], dtype=np.uint8)
+  mask_2[60:len(mask_2)-60,60:len(mask_2)-60] = 0 #mask consisting of the outer frame 60 pixels wide
+  norm_2 = np.sum(mask_2);   #number of non-zero pixels of the thin mask
+  immagine_2 = img * mask_2;   # I just look at the picture frame, the rest is 0
+
+
+  ###########  Test Section  #########
+  if np.sum(immagine_2)/norm_2 <= 2.5 and np.sum(immagine_1)/norm_1 < 103:
+    Test = 2;           # there is a lot of black ----> Photos with a lot of black circular border
+
+  elif np.sum(immagine_1)/norm_1 < 103:
+    Test = 0;           # there is black ----> Photo with black circular border                    
+
+  elif np.sum(immagine_1)/norm_1 >= 103:
+    Test = 1;         # little black around ----> Full Rectangular Photo
+  
+  return Test
+  ####################################
+
+def Labeller (path_in,path_out,class_name):  
+
+  ##### Loop #####
+    
+  directory = os.fsencode(path_in)
+  
+  telescope = 0
+  super_telescope = 0
+  square = 0
+  rectangle = 0
+  
+
+  for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    image = img.imread(path_in+'/'+filename)
+
+    # ----- TEST ------
+ 
+    Test = telescope_test(image) 
+
+    # -----------------
+    if Test == 1:                  # Image without black artifact                        
+      dimensions = image.shape
+      dimensions = dimensions [0:2]              
+      if dimensions[1] >= (dimensions[0] * 1.5):
+        os.rename(path_in+'/'+filename, path_out+'/'+'R_'+class_name+'_'+filename)
+        rectangle = rectangle + 1
+      else:
+        os.rename(path_in+'/'+filename, path_out+'/'+'Q_'+class_name+'_'+filename)
+        square = square + 1 
+    elif Test == 0:                # Image with black artifact
+      os.rename(path_in+'/'+filename, path_out+'/'+'C_'+class_name+'_'+filename)
+      telescope = telescope + 1 
+    elif Test == 2:                # Image with a lot of black artifact
+      os.rename(path_in+'/'+filename, path_out+'/'+'SC_'+class_name+'_'+filename)
+      super_telescope = super_telescope + 1  
+  return rectangle,square,telescope,super_telescope
+  
+  print(str(rectangle)+' images without black artifact (Rectangular images)\n'+str(telescope)+' with black artifact (Telescope images)\n'+str(super_telescope)+' with a lot of black artifact (Super-telescope images)\nwere saved.')
+  print('\n')
+  print('Now lets move on to the next class ...')
+
+def fractional_augmenter(number):
+    number = float(number)
+    frac = number % 1
+    frac = round(frac, 2)
+    integ = int(number)
+    x=random.randint(1, 100)
+    is_true = (x < frac*100)
+
+    if is_true:
+        one_more_time = integ + 1
+    else:
+        one_more_time = integ
+  
+    return one_more_time
+
+def auto_augmenter(r,q,c,sc,tot):
+  if r+q+c+sc == tot:
+    x = 1
+    y = 1
+    z = 1
+  else: 
+    z = (tot-3*r)/(2*q+2*c+sc)
+    x = 2*(tot-3*r)/(2*q+2*c+sc)
+    y = 2*(tot-3*r)/(2*q+2*c+sc)
+  return x,y,z
+
+def detect_group(filename):
+  c = '_'
+  support = [pos for pos, char in enumerate(filename) if char == c]
+  group = filename[0:support[0]]
+  return group
+
+def Classic_Augmentation_Telescope (filename, path, Telescope_multiplicator):
+           
+     image = img.imread(path+'/'+filename)
+# ---------------------------------- INITIAL TRANSFORMATION --------------------
+
+     # Declare transformation : 1° Trasformazione : Rotate + Crop
+     transform = A.Compose([
+              A.Rotate (limit=[45, 45], interpolation=1, border_mode=0, value=None, mask_value=None, rotate_method='largest_box', crop_border=True, always_apply=True, p=1),
+     ]) 
+
+     transformed = transform(image=image)
+     image = transformed["image"]
+     #  --- ADD ---
+     dimension= image.shape
+     height = round((dimension[0]/100)*86)  # new height : 86%
+     width = round((dimension[1]/100)*86)   # new width : 86%
+     #  -----------
+
+
+     Statistical_Telescope = fractional_augmenter(Telescope_multiplicator)
+    
+     for i in range (1, Statistical_Telescope + 1):  # Photos have increased by a factor of 'Telescope_multiplicator'
+       
+       if i==1:
+         transform = A.Compose([                                        
+                  A.CenterCrop (height, width, always_apply=True, p=1), 
+         ])                                                             
+         transformed = transform(image=image)                           
+         transformed_image = transformed["image"]                       
+
+         img.imsave((path+'/'+filename),transformed_image) # Save the first rotated + crop image only  
+
+       else:
+         # Declare transformation: 2nd Transformation. Various augmentation.
+         transform = A.Compose([
+                  A.PiecewiseAffine (scale=(0.025, 0.045), nb_rows=4, nb_cols=4, interpolation=5, mask_interpolation=0, cval=0, cval_mask=0, mode='constant', absolute_scale=False, always_apply=True, keypoints_threshold=0.01, p=1),
+                  A.Flip(always_apply= False, p=0.5),
+                  A.Transpose(always_apply= False, p=0.5),
+                  A.RandomRotate90(always_apply= False, p=0.5),
+                  A.CenterCrop (height, width, always_apply=True, p=1), 
+         ]) 
+
+         transformed = transform(image=image)
+         transformed_image = transformed["image"]
+
+         Pin = str(random.randint(1, 9999))+'_'
+         img.imsave(path+'/'+Pin+str(i)+'_'+filename,transformed_image) # Except subsequent images submitted to Albumentation 
+
+def Classic_Augmentation_Super_Telescope (filename, path, Super_Telescope_multiplicator):
+  
+     image = img.imread(path+'/'+filename)
+
+     # ---------------------------------- INITIAL TRANSFORMATION -----------------
+     # Declare an augmentation pipeline: 1st Transformation: Rotate + Crop
+     transform = A.Compose([
+              A.Rotate (limit=[45, 45], interpolation=1, border_mode=0, value=None, mask_value=None, rotate_method='largest_box', crop_border=True, always_apply=True, p=1),
+     ])  
+     # Augment an image
+     transformed = transform(image=image)
+     image = transformed["image"]
+
+     dimension= image.shape
+     height = round((dimension[0]/100)*72)  # Nuova altezza : 72%
+     width = round((dimension[1]/100)*72)   # Nuova larghezza : 72%
+
+     # Declare an augmentation pipeline: 2°.  Transformation : Center Crop 72 %
+     transform = A.Compose([
+              A.CenterCrop (height, width, always_apply=True, p=1), 
+     ])  
+
+     # Augment an image
+     transformed = transform(image=image)
+     image = transformed["image"]
+
+     # ----------------------------------------------------------------------------
+
+     Statistical_Super_Telescope = fractional_augmenter(Super_Telescope_multiplicator)
+    
+     for i in range (1, Statistical_Super_Telescope + 1):   # Photos have increased by a factor of 'Super_Telescope_multiplicator'
+       
+       if i==1:
+         img.imsave((path+'/'+filename),image) # Saving the first image rotated + crop + centerCrop only
+
+       else:
+          # Declare an augmentation pipeline 3rd Transformation: Various augmentation. Note: The following PiecewiseAffine is more delicate than the others in this block. 
+          transform = A.Compose([
+                  A.PiecewiseAffine (scale=(0.025, 0.045), nb_rows=3, nb_cols=3, interpolation=5, mask_interpolation=0, cval=0, cval_mask=0, mode='constant', absolute_scale=False, always_apply=True, keypoints_threshold=0.01, p=1),
+                  A.Flip(always_apply= False, p=0.5),
+                  A.Transpose(always_apply= False, p=0.5),
+                  A.RandomRotate90(always_apply= False, p=0.5),
+          ])  
+
+          # Augment an image
+          transformed = transform(image=image)
+          transformed_image = transformed["image"]
+
+          Pin = str(random.randint(1, 9999))+'_'
+          img.imsave(path+'/'+Pin+str(i)+'_'+filename,transformed_image) # Saving subsequent images subjected to Albumentation
+
+def Classic_Augmentation_Rectangle (filename, path, Rectangle_multiplicator, current_class):
+
+     image = img.imread(path+'/'+filename)
+
+     ####### Controllo Aspect Ratio #######
+     dimensions = image.shape
+     dimensions = dimensions [0:2]                # I only care about height and width, not the number of channels.
+     if (dimensions[1] >= (dimensions[0] * 1.5)) and (current_class not in Larger_Classes) and (current_class not in Too_many_rectangles):
+       
+       # --- Sliding Windows ---  
+       image_L=image[0:min(dimensions),0:min(dimensions)]
+       image_C=image[0:min(dimensions),round(max(dimensions)/2)-round(min(dimensions)/2):round(max(dimensions)/2)+round(min(dimensions)/2)]
+       image_R=image[0:min(dimensions),max(dimensions)-min(dimensions):max(dimensions)]
+       # -----------------------
+
+       # Declare an augmentation pipeline : Albumentations vari
+       transform = A.Compose([
+               A.PiecewiseAffine (scale=(0.025, 0.045), nb_rows=4, nb_cols=4, interpolation=5, mask_interpolation=0, cval=0, cval_mask=0, mode='constant', absolute_scale=False, always_apply=True, keypoints_threshold=0.01, p=1),
+               A.Flip(always_apply= False, p=0.5),
+               A.Transpose(always_apply= False, p=0.5),
+               A.RandomRotate90(always_apply= False, p=0.5),
+       ])
+
+       # --- Transformations + Saving ---
+       transformed = transform(image=image_L)
+       transformed_image = transformed["image"]
+       label= ('RL_')
+       img.imsave((path+'/'+label+filename),transformed_image)
+      
+       transformed = transform(image=image_C)
+       transformed_image = transformed["image"]
+       label= ('RC_')
+       img.imsave((path+'/'+label+filename),transformed_image)
+
+       transformed = transform(image=image_R)
+       transformed_image = transformed["image"]
+       label= ('RR_')
+       img.imsave((path+'/'+label+filename),transformed_image)
+
+       os.remove(path+'/'+filename)
+       # -----------------------------------
+
+     else:                                                # If, on the other hand, the image is not so rectangular ...                                              
+
+        Statistical_Rectangle = fractional_augmenter(Rectangle_multiplicator)
+        
+        for i in range (1, Statistical_Rectangle + 1):                  # Photos have increased by a factor of 'Rectangular_multiplicator'
+
+         if i==1:
+
+            # Declaration of an augmentation pipeline : Center Crop to make the image exactly square
+           transform = A.Compose([
+                     A.CenterCrop (min(dimensions), min(dimensions), always_apply=True, p=1), 
+           ])  
+           
+           transformed = transform(image=image)
+           transformed_image = transformed["image"]
+
+           img.imsave((path+'/'+filename),transformed_image)
+         else:
+           # Declaration of an augmentation pipeline : Various Albumentations (I perform the elastic twisting before the crop, because in this way the final result is better)
+           transform = A.Compose([
+                     A.PiecewiseAffine (scale=(0.025, 0.045), nb_rows=4, nb_cols=4, interpolation=5, mask_interpolation=0, cval=0, cval_mask=0, mode='constant', absolute_scale=False, always_apply=True, keypoints_threshold=0.01, p=1),
+                     A.Flip(always_apply= False, p=0.5),
+                     A.Transpose(always_apply= False, p=0.5),
+                     A.RandomRotate90(always_apply= False, p=0.5),
+           ])
+           transformed = transform(image=image)
+           transformed_image = transformed["image"]
+
+           new_dimensions = transformed_image.shape
+           new_dimensions = new_dimensions [0:2] 
+           # Declaration of an augmentation pipeline : Center Crop to make the image exactly square
+           transform = A.Compose([
+                     A.CenterCrop (min(new_dimensions), min(new_dimensions), always_apply=True, p=1), 
+           ])  
+           
+           transformed = transform(image=transformed_image)
+           transformed_image = transformed["image"]
+
+           Pin = str(random.randint(1, 9999))+'_'
+           label= ('Q_')
+           img.imsave(path+'/'+Pin+label+str(i)+'_'+filename,transformed_image)
+
+
+def Balance_Dataset(path_in,path_out):
+
+    os.makedirs(path_out, exist_ok = True)
+    directory = os.listdir(path_in)
+    Num_Classes = len(directory)
+    Attributes_Vector = []
+    Larger_Classes = []
+    
+    for i in range(0,Num_Classes):
+      support = counter(path_in+'/'+(directory[i]))
+      Attributes_Vector.append([directory[i],support])
+    
+    support = sorted(Attributes_Vector, key=lambda item: item[1])
+    Largest_Class_Num = support[len(support)-1][1]      # Greater number of all classes.
+    for i in range(0,Num_Classes):
+      if Attributes_Vector[i][1] >= (Largest_Class_Num/100)*95:
+        Larger_Classes.append(Attributes_Vector[i][0])
+    
+    print('Start Sharpening treatment on all classes.')
+    print('\n')
+    
+    # Sharpness Transformation over all classes
+    for i in range(0,Num_Classes):
+      print(directory[i]+' class is processing...')
+      Sharpness(path_in+'/'+(directory[i]),path_out+'/'+(directory[i]))
+     
+    print('\n')
+    print('The scharpening process is finished.')
+    print('\n')
+    print('The test is about to be performed to determine how much circular\nblack artifact the image has. The treatment of each image depends on this test.')
+    
+    print('The test about the black artifact over all classes is processing...')
+    print('\n')
+    
+    details_telescopes = [] # list containing the details on the number of groups concerning the black circular artifact
+    Too_many_rectangles = []
+    
+    for i in range(0,Num_Classes):
+      [rectangle,square,telescope,super_telescope]=Labeller(path_out+'/'+(directory[i]),path_out+'/'+(directory[i]),directory[i])
+      details_telescopes.append([directory[i],rectangle,square,telescope,super_telescope])
+    
+    for i in range(0,Num_Classes):
+      if details_telescopes[i][1] >= ((Attributes_Vector[i][1]/100)*6):
+        Too_many_rectangles.append(directory[i])
+    
+    # details_telescopes:  'Class_name', n° rectangle, n° squares, n° telescope, n° super_telescope
+    
+    print('\n')
+    print('The test of the black artifact on the images is concluded.')
+    
+    print('\n')
+    print('The phase of correction and augmentation of all images of all classes begins.\nThis step will take a while...')
+    
+    # Augmentation & corrections over all classes
+    for i in range(0,Num_Classes):
+          if directory[i] not in Too_many_rectangles:
+            r = details_telescopes[i][1]
+            q = details_telescopes[i][2]
+            c = details_telescopes[i][3]
+            sc = details_telescopes[i][4]
+    
+            print('\n')
+            print(directory[i]+' class is processing...')
+            [Rectangle_multiplicator,Telescope_multiplicator,Super_Telescope_multiplicator] =auto_augmenter(r,q,c,sc,Largest_Class_Num)
+          else:
+            r = 0
+            q = details_telescopes[i][2] + details_telescopes[i][1]
+            c = details_telescopes[i][3]
+            sc = details_telescopes[i][4]
+    
+            print('\n')
+            print(directory[i]+' class is processing...')
+            [Rectangle_multiplicator,Telescope_multiplicator,Super_Telescope_multiplicator] =auto_augmenter(r,q,c,sc,Largest_Class_Num)
+    
+          for file in os.listdir(path_out+'/'+(directory[i])):
+             filename = os.fsdecode(file)
+             group = detect_group(filename)
+    
+             if group == 'R' or group == 'Q':
+               Classic_Augmentation_Rectangle (filename, path_out+'/'+(directory[i]), Rectangle_multiplicator,directory[i])
+    
+             elif group == 'C':
+               Classic_Augmentation_Telescope (filename, path_out+'/'+(directory[i]), Telescope_multiplicator)
+    
+             elif group == 'SC':
+               Classic_Augmentation_Super_Telescope (filename, path_out+'/'+(directory[i]), Super_Telescope_multiplicator)  
+        
+    print('\n')
+    for i in range(0,Num_Classes):
+      print('The size of the '+directory[i]+' class is equal to '+str(counter(path_out+'/'+(directory[i]))))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_in', help='input path of the entire dataset')
+    parser.add_argument('--path_out', help='output path for saving the entire dataset')
+
+
+    args = parser.parse_args()
+    Balance_Dataset(args.path_in,args.path_out)
+
+if __name__ == '__main__':
+    main()
---
+++ Select_Top_Images.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 11 11:52:40 2022
+
+@author: Antonio Vispi
+"""
+
+import argparse
+from skimage.color import rgb2gray
+import numpy as np
+from scipy import ndimage
+import os
+import skimage.io as img
+
+def estimate_sharpness_test(I):
+  
+  I = rgb2gray(I)
+
+  # Get x-gradient in "sx"
+  sx = ndimage.sobel(I,axis=0,mode='constant')
+  # Get y-gradient in "sy"
+  sy = ndimage.sobel(I,axis=1,mode='constant')
+  # Get square root of sum of squares
+  sobel=np.hypot(sx,sy)
+  sharpness = np.sum(sobel)/(sx.shape[0]*sx.shape[1])
+
+  return sharpness
+
+def Top_images(path_in,path_out,top_images_num):
+
+    directory = os.fsencode(path_in)
+      
+    print('Start of selection of the '+top_images_num+' top images ...')
+    list=[]
+    for file in os.listdir(directory):
+      filename = os.fsdecode(file)
+      image = img.imread(path_in+'/'+filename)
+      Sharpness = estimate_sharpness_test(image)
+      Num_pixels = image.shape[0]*image.shape[1]
+      list.append([filename,Num_pixels,Sharpness])
+    
+    
+    # Sort that list by total number of pixels of image
+    byPixels = sorted(list, key=lambda item: item[1])
+    byPixels = byPixels[(len(byPixels)-round(((len(byPixels)-float(top_images_num))/2))-float(top_images_num)):len(byPixels)]
+    
+    # Sort that list by the sharpness score of image
+    bySharpness = sorted(byPixels, key=lambda item: item[2])
+    bySharpness = bySharpness[(len(bySharpness)-float(top_images_num)):len(bySharpness)]
+    
+    print('Start of saving of the '+top_images_num+' top images ...')
+    i=0
+    for i in range (0,len(bySharpness)):
+      image = img.imread(path_in+'/'+bySharpness[i][0])
+      img.imsave(path_out+'/'+bySharpness[i][0],image)
+      i=i+1
+    print(str(i)+' images have been saved in the path_out directory.')
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_in', help='input path of the specific class')
+    parser.add_argument('--path_out', help='output path for saving the specific class')
+    parser.add_argument('--top_images_num', help='Final number of images you want to select')
+
+
+    args = parser.parse_args()
+    Top_images(args.path_in,args.path_out,args.top_images_num)
+
+if __name__ == '__main__':
+    main()
+
